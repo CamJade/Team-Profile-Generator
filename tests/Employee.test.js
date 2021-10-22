2eb50f94f8cb5f7dfe36b82b2f8abb33e54f4f3a
Cameron White <whitcam20@gmail.com>
2021-10-22 00:14:55 -0400

    requiring files

---
+++ tests/Employee.test.js
@@ -0,0 +1,13 @@
+//require from employee file to test
+const { it, expect } = require("@jest/globals");
+const Employee = require("../lib/Employee");
+
+//create tests for employee
+
+//describe test suit first
+discribe('', () => {
+    //tests; description plus callback
+    it('should ', () => {
+        expect()
+    });
+});
