5e0685eacd24706eb011ee38f06ca5bb0ff3a34c
Cameron White <whitcam20@gmail.com>
2021-11-23 21:57:52 -0500

    employee test passes

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -2,12 +2,37 @@
 const { it, expect } = require("@jest/globals");
 const Employee = require("../lib/Employee");
 
-//create tests for employee
+//create tests for employee-name, id, email, role
 
 //describe test suit first
-discribe('', () => {
+describe('Gets Employee name', () => {
     //tests; description plus callback
-    it('should ', () => {
-        expect()
+    it('should return the name', () => {
+        const employeeA = new Employee('Dwight')
+        expect(employeeA.getName()).toBe('Dwight');
+    });
+});
+
+describe('Gets Employee ID', () => {
+    //tests; description plus callback
+    it('should return the Id', () => {
+        const employeeA = new Employee('Dwight', 2)
+        expect(employeeA.getId()).toBe(2);
+    });
+});
+
+describe('Gets Employee email', () => {
+    //tests; description plus callback
+    it('should return the email', () => {
+        const employeeA = new Employee('Dwight', 2, 'email.test@gmail.com')
+        expect(employeeA.getEmail()).toBe('email.test@gmail.com');
+    });
+});
+
+describe('Gets Employee role', () => {
+    //tests; description plus callback
+    it('should return "Employee"', () => {
+        const employeeA = new Employee()
+        expect(employeeA.getRole()).toBe('Employee');
     });
 });
